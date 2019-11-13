07b1bf29a2fae1ef9b66110609e61474e4210be9
Lukas Meißner <meissnerlukas@yahoo.de>
2019-11-13 17:12:15 +0100

    Restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create(name: "Tim Raue", address: "Checkpoint Charlie", phone_number: "03641449950", category: "italian")
+Restaurant.create(name: "Charlotte", address: "Checkpoint Charlie", phone_number: "03641994450", category: "french")
+Restaurant.create(name: "YangYeng", address: "Friedrichstrasse", phone_number: "03641449950", category: "japanese")
+Restaurant.create(name: "Fritte", address: "Kantstraße", phone_number: "03641994450", category: "belgian")
+Restaurant.create(name: "JengJung", address: "Galvanistraße", phone_number: "03641449950", category: "chinese")
